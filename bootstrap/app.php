6c35e2b6aa2bb807609d5238cb821209cb094ea3
Chief-Ely <lawrencepaduatamayo@gmail.com>
2025-10-23 01:12:15 +0800

    Cors refix v2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,7 +21,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Http\Middleware\HandleCors;
+use Illuminate\Http\Middleware\Cors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
