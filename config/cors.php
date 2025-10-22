c071ce400807f08b28a6855424f1f8d2850da27f
Chief-Ely <lawrencepaduatamayo@gmail.com>
2025-10-23 00:47:58 +0800

    2nd commit cors fix

--- config/cors.php
+++ config/cors.php
@@ -6,10 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        '*',
-        '*',
-    ],
+    'allowed_origins' => ['http://localhost:5173', 'https://makimacoffeeshop-production.up.railway.app'],
 
     'allowed_origins_patterns' => [],
 
@@ -19,5 +16,7 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
+
 ];
+
